3085ec6d38b98fa51c7d6fadaf3888b8ded63dca
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-03-04 22:47:23 -0600

    test: add casify function

--- src/utils/__tests__/auth.exercise.extra-2.js
+++ src/utils/__tests__/auth.exercise.extra-2.js
@@ -2,20 +2,27 @@
 import cases from 'jest-in-case';
 import { isPasswordAllowed } from '../auth';
 
+function casify(obj) {
+  return Object.entries(obj).map(([name, { password }]) => ({
+    name: `${password} ${name}`,
+    password,
+  }));
+}
+
 cases(
   'isPasswordAllowed: valid password',
   ({ password }) => expect(isPasswordAllowed(password)).toBe(true),
-  {
+  casify({
     'valid password': {
       password: '!aBc123',
     },
-  },
+  }),
 );
 
 cases(
   'isPasswordAllowed: invalid password',
   ({ password }) => expect(isPasswordAllowed(password)).toBe(false),
-  {
+  casify({
     'to short': {
       password: 'a2c!',
     },
@@ -34,5 +41,5 @@ cases(
     'no non-alphanummeric characters': {
       password: 'ABCdef123',
     },
-  },
+  }),
 );

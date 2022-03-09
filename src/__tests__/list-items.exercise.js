c0b32474884c04e8625756134c8b71e1dd9219af
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-03-08 22:25:04 -0600

    test: rewrite test using inline snapshot

--- src/__tests__/list-items.exercise.js
+++ src/__tests__/list-items.exercise.js
@@ -55,9 +55,10 @@ test('listItem CRUD', async () => {
   expect(dData).toEqual({ success: true });
   const error = await authAPI.get(listItemIdUrl).catch(resolve);
   expect(error.status).toBe(404);
-  expect(error.data).toEqual({
-    message: `No list item was found with the id of ${listItemId}`,
-  });
+  const idMessage = error.data.message.replace(listItemId, 'LIST_ITEM_ID');
+  expect(idMessage).toMatchInlineSnapshot(
+    `"No list item was found with the id of LIST_ITEM_ID"`,
+  );
 });
 
 /* eslint no-unused-vars:0 */
